17d04bb134aedf310541e4730fb6d84bb5d627eb
Eugene <comradeeugene@gmail.com>
2022-01-24 21:36:05 +0300

    start

---
+++ Lesson_6.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
